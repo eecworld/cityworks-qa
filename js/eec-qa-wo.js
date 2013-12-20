4c76109ea2ed05b4d763c30b9f03671f81d05d2e
Steve Shaffer <WatchSteveDrum@gmail.com>
2013-12-20 12:06:46 -0800

    Developing

--- js/eec-qa-wo.js
+++ js/eec-qa-wo.js
@@ -1,19 +1,45 @@
 buildQaPlugin = function() {
 
-  var qaTests = [
-    'tasks',
-    'inspections',
-    'customFields',
-    'requiredFields',
-    'labor',
-    'equipment',
-    'materials'
-  ];
+  var qaTests = {
+    tasks: {
+      status: true,
+      complete: 2,
+      total: 4,
+      description: 'Tasks Complete'
+    },
+    inspections: {
+
+    },
+    customFields: {
+
+    },
+    requiredFields: {
+
+    },
+    labor: {
+
+    },
+    equipment: {
+
+    },
+    materials: {
+
+    }
+  };
 
   for (var i=0; i<qaTests.length; i++) {
-    $('#eec-qa-tests').append(qaTests[i]);
+    $('#eec-qa-tests')
+      .append($('<div class="row" />')
+        .append($('<label class="field label" />')
+          .append(qaTests[i].complete + '/' + qaTests[i].total)
+        )
+        .append($('<div class="field" />')
+          .append($(qaTests[i]).description)
+        )
+      )
+    ;
   }
 
-}
+};
 
 $(window).load(buildQaPlugin());
