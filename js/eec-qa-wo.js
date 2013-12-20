0270fd0b4a099db000d224f2d5e35cbbe8a5c5ec
Steve Shaffer <WatchSteveDrum@gmail.com>
2013-12-20 14:15:08 -0800

    Developing...

--- js/eec-qa-wo.js
+++ js/eec-qa-wo.js
@@ -1,47 +1,139 @@
-var buildQaPlugin = function() {
+var eecQaPlugin = {};
 
-  var qaTests = {
-    tasks: {
-      status: true,
-      complete: 2,
-      total: 4,
-      description: 'Tasks Complete'
-    },
-    inspections: {
+eecQaPlugin.getControlValue = function(controlId) {
+  return $('#' + cw.LayoutManagers.WOGeneral.Controls.get(controlId)).val();
+};
 
-    },
-    customFields: {
+eecQaPlugin.callApi = function(service, method, params, callback) {
+  var url = eecQaPlugin.application + '/services/' + service + '/' + method;
+  var data = { data: JSON.stringify(params) };
+  if (!(service == 'Authentication' && method == 'Authenticate')) {
+    data.token = eecQaPlugin.token['Token'];
+  }
+  $.post(url, data, function(data) {
+    var response = JSON.parse(data);
+    if (response['Status'] == 0) {
+      if (typeof callback === 'function') { callback(data); }
+    } else if (response['Status'] == 1) {
+      alert('Error calling Cityworks API.  ' + response['Message']);
+      if (typeof callback === 'function') { callback(); }
+    } else if (response['Status'] == 2) {
+      //TODO: Re-Authenticate?  Throw unauthorized error?  Does this happen when they just don't have permissions too?
+    }
+  });
+};
 
-    },
-    requiredFields: {
+eecQaPlugin.tests = {
+  tasks: {
+    status: true,
+    complete: 2,
+    total: 4,
+    description: 'Tasks Complete',
+    update: function() {
+      eecQaPlugin.callApi('Tasks', 'ByWorkOrder', {WorkOrderIds: this.workOrderId}, function(data) {
+        console.log(data);
+      })
+    }
+  },
+  inspections: {
 
-    },
-    labor: {
+  },
+  customFields: {
 
-    },
-    equipment: {
+  },
+  requiredFields: {
 
-    },
-    materials: {
+  },
+  labor: {
 
+  },
+  equipment: {
+
+  },
+  materials: {
+
+  }
+};
+
+eecQaPlugin.update = function(tests) {
+
+  if (typeof tests === 'string') { tests = [tests]; }
+
+  var updateTestByName = function(test) {
+    eecQaPlugin.tests[test].update();
+  };
+
+  if (tests == undefined || tests == null) {
+    // Nothing specified.  Update all.
+    for (var test in eecQaPlugin.tests) {
+      if (eecQaPlugin.tests.hasOwnProperty(test)) {
+        updateTestByName(test);
+      }
+    }
+  } else {
+    // Tests specified.  Update by name.
+    for (var i=0; i<tests.length; i++) {
+      if (eecQaPlugin.tests.hasOwnProperty(tests[i])) {
+        updateTestByName(tests[i]);
+      }
+    }
+  }
+  //TODO: Callback?
+};
+
+eecQaPlugin.init = function(params) {
+
+  var authenticate = function(callback) {
+
+    var authWithServer = function(callback) {
+      eecQaPlugin.callApi('Authentication', 'Authenticate', eecQaPlugin['credentials'], function(data) {
+        eecQaPlugin.token = JSON.parse(data)['Value'];
+        localStorage.setItem('eec-qa-token', eecQaPlugin.token);
+        if (typeof callback === 'function') { callback(); }
+      });
+    };
+
+    var token = localStorage.getItem('eec-qa-token');  //TODO: Check token with Authentication/Validate service
+    if (token == null) {
+      authWithServer(callback);
+    } else if (token['Expires'] < new Date()) {  //TODO: Timezone check
+      authWithServer(callback);
+    } else {
+      eecQaPlugin.token = token;
+      if (typeof callback === 'function') { callback(); }
     }
   };
 
-  for (var test in qaTests) {
-    if (qaTests.hasOwnProperty(test)) {
-      $('#eec-qa-tests')
-        .append($('<div class="row" />')
-          .append($('<label class="field label" />')
-            .text(qaTests[test].complete + '/' + qaTests[test].total)
+  var build = function(callback) {
+
+    for (var test in eecQaPlugin.tests) {
+      if (eecQaPlugin.tests.hasOwnProperty(test)) {
+        $(eecQaPlugin.selector)
+          .append($('<div class="row" />')
+            .append($('<label class="field label" />')
+              .text(eecQaPlugin.tests[test].complete + '/' + eecQaPlugin.tests[test].total)
+            )
+            .append($('<div class="field" />')
+              .text(eecQaPlugin.tests[test].description)
+            )
           )
-          .append($('<div class="field" />')
-            .text(qaTests[test].description)
-          )
-        )
-      ;
+        ;
+      }
     }
-  }
+
+    if (typeof callback === 'function') { callback(); }
+
+  };
+
+  eecQaPlugin.application = params['application'];
+  eecQaPlugin.credentials = params['credentials'];
+  eecQaPlugin.selector = params['selector'];
+  eecQaPlugin.workOrderId = eecQaPlugin.getControlValue('cboWorkOrderId');
+
+  authenticate(
+    build(
+      eecQaPlugin.update('tasks')
+    )
+  );
 
 };
-
-buildQaPlugin();
