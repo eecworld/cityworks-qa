d8f8f3a9005a791a2dd5f1d8181712bca660448f
Steve Shaffer <WatchSteveDrum@gmail.com>
2013-12-20 10:46:33 -0800

    Developing

--- js/eec-qa-wo.js
+++ js/eec-qa-wo.js
@@ -2,4 +2,16 @@
  * Created by steve on 12/20/13.
  */
 
-alert('testing wo');
+var qaTests = [
+  'tasks',
+  'inspections',
+  'customFields',
+  'requiredFields',
+  'labor',
+  'equipment',
+  'materials'
+];
+
+for (var i=0; i<qaTests.length; i++) {
+  $('#eec-qa-tests').append(qaTests[i]);
+}
