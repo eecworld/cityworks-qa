c92d156f38a0986e21f0822b32ec7a779a140b70
Steve Shaffer <sshaffer@eecworld.com>
2014-01-09 13:58:33 -0800

    Refactored out WO-specific vs. universal code.

---
+++ js/eec-qa-sr.js
@@ -0,0 +1,89 @@
+//TODO: These are WO-specific.  Make SR-specific.
+eecQaPlugin.tests = {  //TODO: Dynamically specify which tests in init params so they can be assigned per user group through XML?
+  tasks: {
+    description: 'Tasks Complete',
+    update: function() {
+      //TODO: Is this going to run async?
+      eecQaPlugin.callApi('Tasks', 'ByWorkOrder', {WorkOrderIds: [eecQaPlugin.recordId]}, function(data) {
+        var status = '';
+        var complete = 0;
+        var total = data.length;
+        for (var i= 0; i<total; i++) {
+          var task = data[i];
+          if (task['Status'] == 'COMPLETE') { complete++; }
+        }
+        if (complete == total) {
+          status = 'pass'
+        } else {
+          status = 'fail'
+        }
+        eecQaPlugin.setTestResults('tasks', status, complete, total);
+      });
+    }
+  },
+  inspections: {
+    description: 'Inspections Complete',
+    update: function() {
+      //TODO: Write.  It doesn't look like there's going to be an easy way to get "Related Inspections" through the API.
+      //We'll probably have to write our own API to retrieve the relationships.  But getting the status can use the
+      //official one.
+      //OR... we can rely on the work order form to list the related inspections for us (and even their statuses too!)
+    }
+  },
+  customFields: {
+    description: 'Required Custom Fields Filled In',
+    update: function() {
+      //TODO: Write.  Note we'll have to write our own custom fields API because Cityworks doesn't have one (or do it totally cosmetically).
+    }
+  },
+  requiredFields: {
+    description: 'Other Required Fields Filled In',
+    update: function() {
+      //TODO: Write.  This should be able to be done completely cosmetically without any network traffic.  But what about required fields on other pages (i.e. arrived on site)?
+    }
+  },
+  labor: {
+    description: 'Labor Entered',
+    update: function() {
+      eecQaPlugin.callApi('LaborCost', 'WorkOrderCostsByWorkOrder', {WorkOrderIds: [eecQaPlugin.recordId]}, function(data) {
+        var status = '';
+        if (data.length > 0) {  //TODO: More sophisticated check?
+          status = 'pass';
+        } else {
+          status = 'fail';
+        }
+        eecQaPlugin.setTestResults('labor', status);
+      });
+    }
+  },
+  equipment: {
+    description: 'Equipment Entered',
+    update: function() {
+      eecQaPlugin.callApi('EquipmentCost', 'WorkOrderCostsByWorkOrder', {WorkOrderIds: [eecQaPlugin.recordId]}, function(data) {
+        var status = '';
+        if (data.length > 0) {  //TODO: More sophisticated check?
+          status = 'pass';
+        } else {
+          status = 'fail';
+        }
+        eecQaPlugin.setTestResults('equipment', status);
+      });
+    }
+  },
+  materials: {
+    description: 'Materials Entered',
+    update: function() {
+      eecQaPlugin.callApi('MaterialCost', 'WorkOrderCostsByWorkOrder', {WorkOrderIds: [eecQaPlugin.recordId]}, function(data) {
+        var status = '';
+        if (data.length > 0) {  //TODO: More sophisticated check?
+          status = 'pass';
+        } else {
+          status = 'fail';
+        }
+        eecQaPlugin.setTestResults('materials', status);
+      });
+    }
+  }
+};
+
+eecQaPlugin.recordId = eecQaPlugin.getControlValue('cboWorkOrderId');  //TODO: Work with SR
