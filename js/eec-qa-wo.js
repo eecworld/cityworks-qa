52e22842580a5eb9327cf618b594bdd5b7a715de
Steve Shaffer <WatchSteveDrum@gmail.com>
2014-01-11 17:15:24 -0800

    Wrote asset test

--- js/eec-qa-wo.js
+++ js/eec-qa-wo.js
@@ -53,7 +53,7 @@ eecQaPlugin.tests = {  //TODO: Dynamically specify which tests in init params so
         if (data.length == 0) {  //TODO: More sophisticated check?
           status = 'fail';
         } else if (data.legnth == 1) {
-          if (data[0].EntitySid == 0 && data[0].EntityUid == 0) {
+          if (data[0].IsBlank) {
             status = 'fail';
           } else {
             status = 'pass';
