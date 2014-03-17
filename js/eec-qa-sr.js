4d2c3e620ac3ff9647cee012d0c3a898e5ee8460
Steve Shaffer <WatchSteveDrum@gmail.com>
2014-03-17 05:07:23 -0700

    Coding N/A status

--- js/eec-qa-sr.js
+++ js/eec-qa-sr.js
@@ -43,7 +43,7 @@ eecQaPlugin.tests = {  //TODO: Dynamically specify which tests in init params so
         inspIds.push(Number($(this).text()));
       });
       if (inspIds.length == 0) {
-        eecQaPlugin.setTestResults('inspections', 'pass', 0, 0);
+        eecQaPlugin.setTestResults('inspections', 'na', 0, 0);
       } else {
         eecQaPlugin.callApi('Inspection', 'ByIds', {InspectionIds: inspIds}, function(data) {
           var status = '';
@@ -76,7 +76,11 @@ eecQaPlugin.tests = {  //TODO: Dynamically specify which tests in init params so
         if (content != '') { complete++; } //TODO: Don't include date placeholder MM/DD/YYYY
       });
       if (complete == total) {
-        status = 'pass';
+        if (total > 0) {
+          status = 'pass';
+        } else {
+          status = 'na';
+        }
       } else {
         status = 'fail';
       }
